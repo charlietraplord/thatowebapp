a9f089b936547607c49eecc589e8280d92ad2e14
201584979Lerato <Thandokhomotso@gmail.com>
2020-09-30 12:44:52 +0200

    proper working directory

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Noble
+# Thato
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.1.
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DefaultComponent } from "./layouts/default/default.component";
+import { DashboardComponent } from "./modules/dashboard/dashboard.component";
+import { ChatsComponent } from "./modules/chats/chats.component";
+import { CalendarComponent } from "./modules/calendar/calendar.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "",
+        component: DashboardComponent,
+      },
+      {
+        path: "chats",
+        component: ChatsComponent,
+      },
+      {
+        path: "calendar",
+        component: CalendarComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
